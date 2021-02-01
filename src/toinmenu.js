fbf35b53807eb5225725000375a314f29adfcc8d
indra007s20 <76830399+indra007s20@users.noreply.github.com>
2021-02-01 15:57:34 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,14 @@
+const toinmenu = (prefix, pushname) => {
+    return 
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃➢Comandos do Kira
+┣━━━━━━━━━━━━━━━━━━━━━━
+┃⎇${prefix}setprefix
+┃⎇${prefix}block
+┃⎇${prefix}bc
+┃⎇${prefix}bcgc
+┃⎇${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
